49d30431b6711c309226cf91edbc67cf6aba4819
leonardo <martinezjleonardo@gmail.com>
2023-01-28 12:25:46 -0300

    guardado cambios anteriores

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ import chars.charts as graph
 print("Welcome")
 rounds = input("set the plays: ")
 game.run_game(int(rounds))
-#print(game.results.values(),game.results.keys())
 graph.pieChart(game.results.values(),game.results.keys())
